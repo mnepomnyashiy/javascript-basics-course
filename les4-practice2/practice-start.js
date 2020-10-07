27f812164b26f7e0ae0b023c5d68527e2225663e
Mikhail Nepomnyashchiy <michey85@gmail.com>
2020-10-07 16:07:30 +0300

    second practice for les4 added

---
+++ les4-practice2/practice-start.js
@@ -0,0 +1,21 @@
+// 1. Создайте массив из пяти чисел и выведите его элементы
+
+// 2. Дан массив horror. Создайте переменную fourthItem и сохраните в нее четвертое значение из массива. Выведите его на экран.
+const horror = ['Freddy', 'Jason', 'Michael', 'Ghostface', 'Chucky'];
+
+// 3. Дан массив netflixShows. Добавьте в конец массива новое значение. Выведите обновленный массив на экран.
+const netflixShows = ['Orange is the New Black', 'Black Mirror', 'Chewing Gum'];
+
+// 4. Дан массив synonyms. Обойдите его циклом и для каждого значения сфомируйте новую строку с приветствием, типа 'Have a fantastic day!' (где fantastic - элемент исходного массива). Сформированные строки добавьте в массив greetings. Выведите значения массива greetings на экран, обойдя все значения циклом.
+const synonyms = ['fantastic', 'wonderful', 'great'];
+const greetings = [];
+
+// 5. Даны несколько массивов firstNames, lastNames, places. В каждом массиве конкретный индекс относится к одному человеку. Сформируйте новый массив bios, добавив в него приветственные строки, типа "My name is [firstName] [lastName] and I am from [place]". Выведите полученный результат.
+const firstNames = ['Jon', 'Arya', 'Jamie'];
+const lastNames = ['Snow', 'Stark', 'Lannister'];
+const places = ['The Wall', 'Winterfell', 'Kings Landing'];
+
+const bios = [];
+
+// 6. Дан массив increaseByTwo. Обойдите массив, увеличив каждое значение в два раза. Выведите полученный результат.
+const increaseByTwo = [1, 2, 3, 4, 5];
